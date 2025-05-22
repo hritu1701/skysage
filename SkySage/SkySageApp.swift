7f3ac7d25bbe0ab9854de86551e3f5142827e635
Hritu Raj <sac3xhin@gmail.com>
2025-05-22 23:44:53 +0530

    Initial Commit

---
+++ SkySage/SkySageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkySageApp.swift
+//  SkySage
+//
+//  Created by Hritu Raj on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SkySageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
