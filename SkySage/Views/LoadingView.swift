f8b120674f6a6f36d5f9289605651aeef20173f2
Hritu Raj <sac3xhin@gmail.com>
2025-06-03 16:16:14 +0530

    All done

---
+++ SkySage/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  SkySage
+//
+//  Created by Hritu Raj on 23/05/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ SkySage/Views/WelcomeView.swift
@@ -0,0 +1,38 @@
+//
+//  WelcomeView.swift
+//  SkySage
+//
+//  Created by Hritu Raj on 22/05/25.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    var body: some View {
+        VStack{
+            VStack(spacing:20){
+                Text("Welcome to SkySage")
+                    .bold() .font(.title)
+                Text("Please select your current location to get the weather in your area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation){
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
